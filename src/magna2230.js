426d5b3129cf71e619f6f0c37ef061f4ccd2e608
ferdeangeli <fer.deangeli@gmail.com>
2022-08-25 23:55:13 -0300

    modificación magna2230.js

--- src/magna2230.js
+++ src/magna2230.js
@@ -1,25 +1,18 @@
 
 function agregarCarrito(e){
     e.preventDefault()
+    let listaCompras = localStorage.getItem("carrito") || [];
+    localStorage.removeItem("carrito");
 
     const descripcion = "Hellbot Magna 2 230"
-    const cantidad = parseInt(document.querySelector("#cantidadMagna2230").value)
-    const precio = parseFloat(document.querySelector("#precioMagna2230").value)
-    const subtotal = cantidad * precio  
+    let cantidad = parseInt(document.querySelector("#cantidadMagna2230").value)
+    const precio = parseFloat(document.querySelector("#precioMagna2230").textContent)
+    let subtotal = cantidad * precio
 
-    const listaCompras = localStorage.getItem("carrito") || [];
-    localStorage.removeItem("carrito");
-    function nuevaLista(){
-        listaCompras.push(descripcion, cantidad, precio, subtotal);
-        const listaComprasJSON = JSON.stringify(listaCompras);
-        localStorage.setItem("carrito", listaComprasJSON);
-    }
-    nuevaLista()
-    
-    console.log(listaCompras)
-    console.log(parseInt(cantidad))
-    console.log(precio)
-    console.log(subtotal)
+    listaCompras.push(descripcion, cantidad, precio, subtotal);
+    let listaComprasJSON = JSON.stringify(listaCompras);
+    localStorage.setItem("carrito", listaComprasJSON);
+     
 }
 
 const botonCarrito = document.querySelector("#agregarCarritoMagna2230")
