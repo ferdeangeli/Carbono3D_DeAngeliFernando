0cf4a5a3aba3ca3e85bd6eb8f86dd0e5cc810e3f
ferdeangeli <fer.deangeli@gmail.com>
2022-08-31 00:04:10 -0300

    se arma carrito desde localstorage y se agregan alertas con tostify y sweetalert

--- src/carritofinal.js
+++ src/carritofinal.js
@@ -1,30 +1,31 @@
-let listaCompras = JSON.parse(localStorage.getItem("carrito")) || []
+const listaCompras = JSON.parse(localStorage.getItem("carrito")) || []
 console.log(listaCompras)
 
 //Aca arma la tabla del carrito fila por fila
 
-function tablaCarrito (array = []){
+function tablaCarrito (){
     const tbody = document.querySelector("#filasCarrito")
-    
+    num = 0
+
     for (const item of listaCompras){
-        num = 0
+       
         num++
         const tr = document.createElement("tr");
-        tr.innerHTML = '<td>num</td> <td>${item.descripcion}</td> <td>${item.cantidad}</td> <td>${item.precio}</td> <td>${item.subtotal}</td>';
+        tr.innerHTML = `<td>${num}</td>
+                        <td style="text-align:left">${item.descripcion}</td>
+                        <td>${item.cantidad}</td>
+                        <td>${item.precio}</td>
+                        <td>$${item.subtotal}</td>
+                        <td><input class="actualizarCantidadCarrito form-control" type="number" value=${item.cantidad}></input></td>
+                        <td><button class="botonEliminarCarrito"><img src="../assets/img/tachito.png"></img></button></td>`
+
         tbody.appendChild(tr)
+
     }
 
-    /* array.forEach( (producto) => {
-        let num = 0
-        num++
-        let descripcion = producto.descripcion
-        const tr = document.createElement("tr")
-        tr.innerHTML = '<td>"descripcion"</td>'
-        tbody.appendChild(tr);
-    }); */
-}
+   }
 
-tablaCarrito(listaCompras)
+tablaCarrito()
 
 
 //Aca calcula el total de lo que hay en el carrito
@@ -33,13 +34,13 @@ tablaCarrito(listaCompras)
 let subtotales = listaCompras.map((el) => el.subtotal)
 let total = 0
 
-function totalizar(arr, fn){
+const totalizar = (arr, fn) => {
     for (const el of arr){
         fn(el)
     }
 }
 
-function sumar(subt){
+const sumar = (subt) => {
     total += subt
 }
 
@@ -47,4 +48,15 @@ totalizar(subtotales, sumar)
 
 montoTotal = document.querySelector("#totalCarrito")
 montoTotal.innerText = "$" + total
+
+//Checkout
+
+const finalizarCompra = (e) => {
+    e.preventDefault()
+    swal("¡Muchas gracias por su Compra!", "Enviaremos su factura por email", "success")
+    localStorage.removeItem("carrito")
+}
+
+botonComprar = document.querySelector("#botonComprar")
+botonComprar.addEventListener("click", finalizarCompra)
 
